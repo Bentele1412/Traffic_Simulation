5d6e5b53bb0cc7c74ddd2ef2cc643b4ca1178227
Bentele1412 <marcus.bentele@web.de>
2021-10-25 10:34:03 +0200

    Mean speed plotting

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Traffic Simulation Project
 This is the README.md of the repository "Traffic_Simulation".
+
+## File generation
+To generate flow.xml files, run e.g.
+
+```
+cd "C:\Program Files (x86)\Sumo\tools"
+randomTrips.py -n circularControlNet.net.xml -o circularControlFlow.xml --begin 0 --end 36000 --random --binomial 1 -p 6
+```
+
+To generate the corresponding routes, run e.g.
+
+```
+cd "C:\Users\Marcus\Desktop\Hauptordner\Studium\Masterstudium\3. Semester\Projekt_Sim-Opt\Traffic_Simulation\ZUS_Test"
+jtrrouter -c circularControl.jtrrcfg
+```
---
+++ ZUS_Test/singleCrosswayRunnerGreenPhasePlotting.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import print_function
+
+import traci
+import sys
+import os
+from sumolib import checkBinary
+import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import numpy as np
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+def getMeanSpeed():
+    tree = ET.parse("statistics.xml")
+    root = tree.getroot()
+    return root.find('vehicleTripStatistics').attrib['speed']
+
+def changeDuration(duration):
+    tree = ET.parse("circularControlNet.net.xml")
+    root = tree.getroot()
+    for counter, phase in enumerate(root.iter("phase")):
+        if counter % 2 == 0:
+            phase.set("duration", str(duration))
+    tree.write("circularControlNet.net.xml")
+
+def run():
+    step = 0
+    traci.trafficlight.setPhase("m", 0)
+    detectedVehicles = 0
+    lastVehicleIDs = []
+    while traci.simulation.getMinExpectedNumber() > 0:
+        traci.simulationStep()
+        '''
+        #detectedVehicles += traci.inductionloop.getLastStepVehicleNumber("detect")
+        vehicleIDs = traci.inductionloop.getLastStepVehicleIDs("detect")
+        for id in vehicleIDs:
+            if not id in lastVehicleIDs:
+                detectedVehicles += 1
+        lastVehicleIDs = vehicleIDs
+        '''
+        step += 1
+    #print("Vehicles passed detector: ", detectedVehicles)
+    traci.close()
+    sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    sumoBinary = checkBinary('sumo')
+    configPath = os.path.abspath("circularControl.sumocfg")
+    netPath = os.path.abspath("circularControlNet.net.xml")
+
+    #optimization loop
+    greenDuration = 0
+    direction = 1
+    changeDuration(greenDuration)
+    maxGreenDuration = 51
+
+    meanSpeeds = np.zeros(maxGreenDuration, dtype=float)
+
+    for i in range(1, maxGreenDuration+1):
+        greenDuration += direction
+        changeDuration(greenDuration)
+
+        #start server
+        traci.start([sumoBinary, "-c", configPath,
+                                "--tripinfo-output", "tripinfo.xml",
+                                "--statistic-output", "statistics.xml"])
+
+        #one simulation run
+        run()
+
+        meanSpeeds[i-1] = getMeanSpeed()
+        print("Green duration: ", greenDuration)
+    
+    greenDurations = np.arange(1, maxGreenDuration+1, 1)
+    plt.plot(greenDurations, meanSpeeds)
+    plt.yticks(np.arange(1, 10, 1))
+    plt.title("Mean speeds of various green phase durations")
+    plt.xlabel("Green phase duration")
+    plt.ylabel("Mean speed")
+    plt.show()
