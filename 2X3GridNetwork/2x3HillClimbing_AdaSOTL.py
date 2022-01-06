2eb50bb7ff0e0dc5d72cd9a213fc71465366a770
Bentele1412 <marcus.bentele@web.de>
2022-01-06 14:07:31 +0100

    HillClimbing optimization AdaSOTL

--- 2X3GridNetwork/2x3HillClimbing_AdaSOTL.py
+++ 2X3GridNetwork/2x3HillClimbing_AdaSOTL.py
@@ -3,11 +3,11 @@
 from helperTwoPhases import *
 
 if __name__ == '__main__':
-    alpha = 5
-    beta = 1.5
+    alpha = 4.182
+    beta = 1.1866
     evalFunc = meanSpeedAdaSOTL
     
     params = [alpha, beta]
-    stepSizes = [1, 0.2]
+    stepSizes = [0.1, 0.025]
     hillClimbing = HillClimbing(evalFunc, params, stepSizes)
-    hillClimbing.optimize(epsilon=0.1, maxIter=5, numRuns=1, strategy=1)
+    hillClimbing.optimize(epsilon=0.01, maxIter=50, numRuns=5, strategy=1)
