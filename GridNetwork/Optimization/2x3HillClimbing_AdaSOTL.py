311cb2a5e60e748f877e5bb91212da987d599518
Bentele1412 <marcus.bentele@web.de>
2022-01-19 17:14:20 +0100

    Modularize project

---
+++ GridNetwork/Optimization/2x3HillClimbing_AdaSOTL.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import sys
+sys.path.insert(0, "../")
+
+from optimizers.HillClimbing import HillClimbing
+from additionalFuncs.evaluation import meanSpeedAdaSOTL
+
+if __name__ == '__main__':
+    alpha = 3
+    beta = 1.4
+    evalFunc = meanSpeedAdaSOTL
+    
+    params = [alpha, beta]
+    stepSizes = [0.5, 0.05]
+    hillClimbing = HillClimbing(evalFunc, params, stepSizes)
+    hillClimbing.optimize(epsilon=0.001, maxIter=100, numRuns=5, strategy=1)
---
+++ GridNetwork/additionalFuncs/evaluation.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+
+import traci
+import sys
+import os
+import numpy as np
+from sumolib import checkBinary
+from additionalFuncs.helper import mapLPDetailsToTL, getTLPhaseInfo, getMeanSpeedWaitingTime, createTrafficLights, setFlows
+from controllers.CycleBasedTLController import CycleBasedTLController
+from controllers.AdaSOTL import AdaSOTL
+from controllers.SOTL import SOTL
+
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+
+def meanSpeedCycleBased(params):
+    def _run(trafficLights, ctFactor, phaseShifts, lpSolveResultPaths):
+        step = 0
+        pathCounter = 0
+        cycleBasedTLControllers = []
+        while traci.simulation.getMinExpectedNumber() > 0:
+            if step % 1200 == 0 and step < 3600:
+                mapLPDetailsToTL(trafficLights, lpSolveResultPaths[pathCounter])
+                maxNodeUtilization = max([tl.utilization for tl in trafficLights])
+                numPhases, yellowPhaseDuration = getTLPhaseInfo()
+                cycleTime = int(np.round(ctFactor * ((1.5 * (numPhases/2)*yellowPhaseDuration + 5)/(1 - maxNodeUtilization)))) #maybe edit hard coded yellow phases and extract them from file
+                pathCounter += 1
+                if step == 0:
+                    for counter, tl in enumerate(trafficLights):
+                        cycleBasedTLControllers.append(CycleBasedTLController(tl, cycleTime, phaseShifts[counter], numPhases, yellowPhaseDuration))
+                else:
+                    for counter, controller in enumerate(cycleBasedTLControllers):
+                        controller.setCycle(cycleTime, phaseShifts[counter])
+            
+            for controller in cycleBasedTLControllers:
+                controller.step()
+            traci.simulationStep()
+
+            step += 1
+        traci.close()
+        sys.stdout.flush()
+
+    sumoBinary = checkBinary('sumo')
+    configPath = os.path.abspath("../2x3.sumocfg")
+    simulationTime = 3600
+    numVehicles = 900
+    ctFactor = params[0]
+    #phaseShifts = [0]*6 #6 for 6 junctions 
+    phaseShifts = [0] + list(map(lambda x: int(x), params[1:]))
+    lpSolveResultPaths = ['../LPSolve/2x3Grid_a_eps0,4.lp.csv', '../LPSolve/2x3Grid_b_eps0,4.lp.csv', '../LPSolve/2x3Grid_c_eps0,4.lp.csv']
+
+    #create instances
+    trafficLights = createTrafficLights()
+
+    setFlows(numVehicles, simulationTime)
+    os.system('jtrrouter -c ../2x3.jtrrcfg')
+
+    traci.start([sumoBinary, "-c", configPath,
+                                    "--tripinfo-output", "../tripinfo.xml",
+                                    "--statistic-output", "../statistics.xml"])
+
+    _run(trafficLights, ctFactor, phaseShifts, lpSolveResultPaths)
+
+    meanSpeed, meanWaitingTime = getMeanSpeedWaitingTime()
+    return float(meanSpeed)
+
+def meanSpeedAdaSOTL(params):
+    def _run(adaSotls):
+        step = 0
+        while traci.simulation.getMinExpectedNumber() > 0:
+            traci.simulationStep()
+            for sotl in adaSotls:
+                sotl.step()
+            step += 1
+        traci.close()
+        sys.stdout.flush()
+
+    sumoBinary = checkBinary('sumo')
+    configPath = os.path.abspath("../2x3.sumocfg")
+    simulationTime = 3600
+    numVehicles = 900
+
+    #create instances
+    minGreenTime = 20
+    maxGreenTime = 55 #change to maxRedTime
+    trafficLights = createTrafficLights(minGreenTime, maxGreenTime)
+
+    mu = 3
+    
+    beta = params[1]
+    alpha = params[0]
+    adaSotls = []
+    for tl in trafficLights:
+        adaSotls.append(AdaSOTL(tl, mu, alpha, beta))
+
+    setFlows(numVehicles, simulationTime)
+    os.system('jtrrouter -c ../2x3.jtrrcfg')
+
+    traci.start([sumoBinary, "-c", configPath,
+                                    "--tripinfo-output", "../tripinfo.xml",
+                                    "--statistic-output", "../statistics.xml"])
+    
+    _run(adaSotls)
+
+    meanSpeed, meanWaitingTime = getMeanSpeedWaitingTime()
+    return float(meanSpeed)
+
+def meanSpeedSOTL(params):
+    def _run(sotls):
+        step = 0
+        while traci.simulation.getMinExpectedNumber() > 0:
+            traci.simulationStep()
+            for sotl in sotls:
+                sotl.step()
+            step += 1
+        traci.close()
+        sys.stdout.flush()
+
+    sumoBinary = checkBinary('sumo')
+    configPath = os.path.abspath("../2x3.sumocfg")
+    simulationTime = 3600
+    numVehicles = 900
+
+    #create instances
+    minGreenTime = 20
+    maxGreenTime = 55 #change to maxRedTime
+    trafficLights = createTrafficLights(minGreenTime, maxGreenTime)
+
+    mu = 3
+    
+    theta = params[0]
+    sotls = []
+    for tl in trafficLights:
+        sotls.append(SOTL(tl, mu, theta))
+
+    setFlows(numVehicles, simulationTime)
+    os.system('jtrrouter -c ../2x3.jtrrcfg')
+
+    traci.start([sumoBinary, "-c", configPath,
+                                    "--tripinfo-output", "../tripinfo.xml",
+                                    "--statistic-output", "../statistics.xml"])
+    
+    _run(sotls)
+
+    meanSpeed, meanWaitingTime = getMeanSpeedWaitingTime()
+    return float(meanSpeed)
