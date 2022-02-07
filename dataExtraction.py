17162d398c40e7e9d6942a6f1dc99ad865936b72
Bentele1412 <marcus.bentele@web.de>
2022-02-07 06:35:15 +0100

    Several experiment results

--- dataExtraction.py
+++ dataExtraction.py
@@ -2,7 +2,7 @@ import pickle
 import numpy as np
 
 if __name__ == '__main__':
-    pathToFile = "./GridNetwork/Plots/HillClimbing_AdaSOTL_2x3_5runs_strat1_1500veh/dynamicsData.pickle"
+    pathToFile = "./GridNetwork/Plots/HillClimbing_AdaSOTL_2x3_5runs_strat1_900veh_corrected/dynamicsData.pickle"
     with open(pathToFile, "rb") as f:
         data = pickle.load(f)
     maxSpeedInd = np.argmax(data['meanSpeed'])
